2d723bdde197d283c0cf6d5a246fe9b7dbed85b4
Mayuresh9890 <mayuresh.kapdule@gmail.com>
2017-07-25 12:57:42 +0530

    design change

--- AndroidSpinnerEg/app/src/main/java/com/example/admin/androidspinnereg/MainActivity.java
+++ AndroidSpinnerEg/app/src/main/java/com/example/admin/androidspinnereg/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.admin.androidspinnereg;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final int EXTERNAL_STORAGE_PERMISSION_CONSTANT = 100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.spinner_activity);
     }
+
+
 }
--- AndroidSpinnerEg/app/src/main/java/com/example/admin/androidspinnereg/SpinnerJava.java
+++ AndroidSpinnerEg/app/src/main/java/com/example/admin/androidspinnereg/SpinnerJava.java
@@ -5,12 +5,18 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.telephony.TelephonyManager;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -18,20 +24,29 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by admin on 17/07/2017.
  */
 
-public class SpinnerJava extends Activity {
+public class SpinnerJava extends AppCompatActivity {
     String[] country = { "India", "USA", "China", "Japan", "Other",  };
     private static final int EXTERNAL_STORAGE_PERMISSION_CONSTANT = 100;
+    private static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 1;
     private SharedPreferences permissionStatus;
     private static final int REQUEST_PERMISSION_SETTING = 101;
     private boolean sentToSettings = false;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.spinner_activity);
-        checkPermissionTest();
+        if (requestPermission()) {
+            Toast.makeText(this, "Permissions Granted", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Permissions Issues", Toast.LENGTH_SHORT).show();
+        }
+        //checkPermissionTest();
         //Getting the instance of Spinner and applying OnItemSelectedListener on it
         Spinner spin = (Spinner) findViewById(R.id.Testspinner);
 
@@ -52,35 +67,32 @@ public class SpinnerJava extends Activity {
         Toast.makeText(getApplicationContext(),country[position] ,Toast.LENGTH_LONG).show();
     }
 
-    public String checkPermissionTest(){
-        String result="";
-        // Here, thisActivity is the current activity
-        if (ContextCompat.checkSelfPermission(SpinnerJava.this, Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) {
-            //Toast.makeText(getApplicationContext(),"PERMISSION_GRANTED1235",Toast.LENGTH_SHORT).show();
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(SpinnerJava.this,Manifest.permission.READ_PHONE_STATE)) {
-                //if(true){
-                //Toast.makeText(getApplicationContext(),"PERMISSION_GRANTED1234",Toast.LENGTH_SHORT).show();
-                AlertDialog.Builder builder = new AlertDialog.Builder(SpinnerJava.this);
-                builder.setTitle("Need Storage Permission");
-                builder.setMessage("This app needs storage permission.");
-                builder.setPositiveButton("Grant", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-                        ActivityCompat.requestPermissions(SpinnerJava.this, new String[]{Manifest.permission.READ_PHONE_STATE},EXTERNAL_STORAGE_PERMISSION_CONSTANT);
-                    }
-                });
-                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-                    }
-                });
-                builder.show();
 
-            }
+    private boolean requestPermission() {
+        int sdcardPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int networkPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET);
+        int statusPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE);
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        if (sdcardPermission != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
         }
-        return result;
+        if (networkPermission != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.INTERNET);
+        }
+        if (statusPermission != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.READ_PHONE_STATE);
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), REQUEST_ID_MULTIPLE_PERMISSIONS);
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.spinner_menu, menu);
+        return true;
     }
 }
